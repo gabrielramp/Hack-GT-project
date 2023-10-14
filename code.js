db6d844935bb62f38ac353f0edd511282dd9614f
shanmathig <shanmathi7215@gmail.com>
2023-10-14 04:24:34 -0400

    add loaading to days js
    add loaading to days js

---
+++ code.js
@@ -0,0 +1,11 @@
+// code.js
+
+// Simulating a delay for backend data retrieval (you can replace this with actual data loading)
+setTimeout(function () {
+    redirectToDayPage();
+}, 3000); // Replace 3000 with the actual delay in milliseconds
+
+function redirectToDayPage() {
+    // Redirect to the "day.html" page
+    window.location.href = "day.html";
+}
