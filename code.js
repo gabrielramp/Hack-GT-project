f332c3df8f4c6cff8672d017172826e93bf9015a
shanmathig <shanmathi7215@gmail.com>
2023-10-14 12:25:06 -0400

    added weekly formt
    added weekly formt

--- code.js
+++ code.js
@@ -1,32 +1,55 @@
-// code.js
-
-// Simulating a delay for backend data retrieval (you can replace this with actual data loading)
-// setTimeout(function () {
-//     redirectToDayPage();
-// }, 3000); // Replace 3000 with the actual delay in milliseconds
-
 function redirectToDayPage() {
     // Redirect to the "day.html" page
     window.location.href = "days.html";
 }
+
 // script.js
+
+function generatePage() {
+    // Get the values from the input fields and store them in variables
+    const skill = document.getElementById("input1").value;
+    const hoursPerDay = document.getElementById("input2").value;
+    const numOfDays = document.getElementById("input3").value;
+    // Redirect to the "loading.html" page
+    window.location.href = "loading.html";
+
+    // You can use the 'skill', 'hoursPerDay', and 'numOfDays' variables as needed
+    // For example, you can pass them to the 'loading.html' page or perform other operations.
+}
+
+// Function to create boxes
+// script.js
+
 // code.js
-// script.js
+
+// ...
 
 // Function to create boxes
 function createBoxes() {
-    // Get the value of the input field
-    const numDays = document.getElementById("numDays").value;
-
-    // Get the box container
+    const numDays = parseInt(document.getElementById("numDays").value);
     const boxContainer = document.getElementById("boxContainer");
+    boxContainer.innerHTML = "";
 
-    // Create and append boxes
     for (let i = 1; i <= numDays; i++) {
         const box = document.createElement("div");
         box.className = "box";
+        box.textContent = "Day " + i;
+
+        // Add a click event to open the day detail
+        box.addEventListener("click", function () {
+            const dayDetail = document.getElementById("dayDetail");
+            dayDetail.style.display = "block";
+            const dayDetailContent = document.getElementById("dayDetailContent");
+            dayDetailContent.textContent = "Details for Day " + i;
+
+            const closeBtn = document.getElementById("closeBtn");
+            closeBtn.addEventListener("click", function () {
+                dayDetail.style.display = "none";
+            });
+        });
+
         boxContainer.appendChild(box);
     }
 }
 
-
+// ...
