6687a6a47708a9ea3dad46ceadbb251695e7b7e6
Rishabh Kanodia <55036111+RishabhK9@users.noreply.github.com>
2023-10-14 15:09:27 -0400

    add editable number of days

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const openai = new OpenAI({
 });
 
 var numDays = 10;
-var prompt = "Give me a plan with resources to learn Java in 10 days in a table format. The table should have four columns. The first column should include the day number (say Day # in each row), the second column should include the topic, the third column should include resources with specific links and titles, and the fourth column should include the tasks. The first column should only include the number of the day. The second column should only include the topic to be learned. The third column should only include recommended resources to learn that topic. The fourth column should only include the tasks that should be taken to learn the corresponding topic for each day. Please surround strings with double quotes. Set the temperature setting to 0.1.";
+var prompt = "Give me a plan with resources to learn Java in " + numDays + " days in a table format. The table should have four columns. The first column should include the day number (say Day # in each row), the second column should include the topic, the third column should include resources with specific links and titles, and the fourth column should include the tasks. The first column should only include the number of the day. The second column should only include the topic to be learned. The third column should only include recommended resources to learn that topic. The fourth column should only include the tasks that should be taken to learn the corresponding topic for each day. Please surround strings with double quotes. Set the temperature setting to 0.1.";
 
 const chatCompletion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
