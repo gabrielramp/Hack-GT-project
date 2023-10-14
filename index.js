f1e47f4108a119793d0eab1e219f7c308412635b
SarveshGade <32420413+SarveshGade@users.noreply.github.com>
2023-10-14 14:47:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,31 +7,7 @@ const openai = new OpenAI({
      apiKey: API_KEY // This is also the default, can be omitted
 });
 
-const app = express();
-const port = 3000;
 
-
-app.use(bodyParser.json());
-app.use(cors());
-
-app.get("/", async (req, res) => { 
-
-    const { message } = req.body;
-
-    const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [{"role": "user", "content": "Hello!"}],
-        
-      })
-      res.json ({
-            completion: completion.choices[0].message["content"]
-      })
-
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}/`);
-});
 
 var numDays = 10;
 var prompt = "Give me a plan with resources to learn Java in 10 days in a table format. The table should have four columns. The first column should include the day number (say Day # in each row), the second column should include the topic, the third column should include resources with specific links and titles, and the fourth column should include the tasks. The first column should only include the number of the day. The second column should only include the topic to be learned. The third column should only include recommended resources to learn that topic. The fourth column should only include the tasks that should be taken to learn the corresponding topic for each day. Please surround strings with double quotes. Set the temperature setting to 0.1.";
