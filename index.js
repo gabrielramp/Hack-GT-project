81169aea73c18a653d2f78312cf3ce610362b39e
Rishabh Kanodia <55036111+RishabhK9@users.noreply.github.com>
2023-10-14 18:15:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-     apiKey: "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" // This is also the default, can be omitted
+     apiKey: API_KEY // This is also the default, can be omitted
 });
 
 var skillTopic = "Java";
 var numDays = 10;
-var prompt = "Give me a plan with resources to learn " + skillTopic + " in " + numDays + " days in a table format. The table should have four columns. The first column should include the day number (say Day # in each row), the second column should include the topic, the third column should include resources with specific links and titles, and the fourth column should include the tasks. The first column should only include the number of the day. The second column should only include the topic to be learned. The third column should only include recommended resources to learn that topic. The fourth column should only include the tasks that should be taken to learn the corresponding topic for each day. Please surround strings with double quotes. Set the temperature setting to 0.1.";
+var prompt = "Give me a plan with resources to learn " + skillTopic + " in " + numDays + " days in a table format. The table should have four columns. The first column should include the day number (say Day # in each row), the second column should include the topic, the third column should include multiple resources with specific links and titles, and the fourth column should include multiple tasks. The first column should only include the number of the day. The second column should only include the topic to be learned. The third column should only include multiple recommended resources to learn that topic. The fourth column should only include the multiple tasks that should be taken to learn the corresponding topic for each day. Give an equal amount of tasks and resources but make sure to give multiple. Please surround strings with double quotes. Please use real links. Set the temperature setting to 0.1.";
 
 const chatCompletion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
