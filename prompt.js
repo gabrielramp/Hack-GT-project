47c18960b15441a38f3c0ef8a241fc4a44115b83
Rishabh Kanodia <55036111+RishabhK9@users.noreply.github.com>
2023-10-15 01:15:49 -0400

    push

--- prompt.js
+++ prompt.js
@@ -1,17 +1,14 @@
 import OpenAI from 'openai';
 
 class RoadMapper {
-  static async processChat() {
+  async processChat() {
       const openai = new OpenAI({
-          apiKey: API_KEY
+          apiKey: "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"
       });
-      
+      console.log("iterating processChat");
       var skillTopic = "Java";
       var numDays = 10;
       var prompt = "Give me a plan with resources to learn " + skillTopic + " in " + numDays + " days in a table format. The table should have four columns. The first column should include the day number (say including the word Day in each row), the second column should include the topic, the third column should include multiple resources with specific links and titles, and the fourth column should include multiple tasks. The second column should only include the topic to be learned. The third column should only include multiple recommended resources to learn that topic. The fourth column should only include the multiple tasks that should be taken to learn the corresponding topic for each day. Give an equal amount of tasks and resources but make sure to give multiple. Please surround strings with double quotes. Please use real links. Set the temperature setting to 0.1.";
-      prompt = "Hi! Tell me a number."
-      
-      console.log("Output is generating")
 
       const chatCompletion = await openai.chat.completions.create({
           model: "gpt-3.5-turbo",
@@ -25,7 +22,7 @@ class RoadMapper {
           let result = separatedOutput[i].split("|");
           for (let j = 0; j < result.length; j++) {
               result[j] = result[j].trim();
-              if (result[j] === "" || result[j] === '') {
+              if (result[j].trim() === "" || result[j].trim() === '') {
                   result.splice(j, 1);
               }
           }
